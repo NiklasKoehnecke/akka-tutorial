b6705cdc251efd55040434e203d73e2bc824a237
Niklas Koehnecke <niklas.koehnecke@gmail.com>
2021-05-15 21:42:19 +0200

    Finish LargeMessageProxy

--- ddm-exercise/src/main/java/de/hpi/ddm/actors/LargeMessageProxy.java
+++ ddm-exercise/src/main/java/de/hpi/ddm/actors/LargeMessageProxy.java
@@ -1,91 +1,177 @@
 package de.hpi.ddm.actors;
 
 import java.io.Serializable;
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.CompletionStage;
+import java.util.concurrent.ExecutionException;
 
+import akka.NotUsed;
 import akka.actor.AbstractLoggingActor;
 import akka.actor.ActorRef;
 import akka.actor.ActorSelection;
 import akka.actor.Props;
+import akka.stream.javadsl.Sink;
+import akka.stream.javadsl.Source;
+import com.twitter.chill.KryoPool;
+import de.hpi.ddm.singletons.KryoPoolSingleton;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 public class LargeMessageProxy extends AbstractLoggingActor {
 
-	////////////////////////
-	// Actor Construction //
-	////////////////////////
+    ////////////////////////
+    // Actor Construction //
+    ////////////////////////
 
-	public static final String DEFAULT_NAME = "largeMessageProxy";
-	
-	public static Props props() {
-		return Props.create(LargeMessageProxy.class);
-	}
+    public static final String DEFAULT_NAME = "largeMessageProxy";
 
-	////////////////////
-	// Actor Messages //
-	////////////////////
-	
-	@Data @NoArgsConstructor @AllArgsConstructor
-	public static class LargeMessage<T> implements Serializable {
-		private static final long serialVersionUID = 2940665245810221108L;
-		private T message;
-		private ActorRef receiver;
-	}
+    public static Props props() {
+        return Props.create(LargeMessageProxy.class);
+    }
 
-	@Data @NoArgsConstructor @AllArgsConstructor
-	public static class BytesMessage<T> implements Serializable {
-		private static final long serialVersionUID = 4057807743872319842L;
-		private T bytes;
-		private ActorRef sender;
-		private ActorRef receiver;
-	}
-	
-	/////////////////
-	// Actor State //
-	/////////////////
-	
-	/////////////////////
-	// Actor Lifecycle //
-	/////////////////////
+    ////////////////////
+    // Actor Messages //
+    ////////////////////
 
-	////////////////////
-	// Actor Behavior //
-	////////////////////
-	
-	@Override
-	public Receive createReceive() {
-		return receiveBuilder()
-				.match(LargeMessage.class, this::handle)
-				.match(BytesMessage.class, this::handle)
-				.matchAny(object -> this.log().info("Received unknown message: \"{}\"", object.toString()))
-				.build();
-	}
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class LargeMessage<T> implements Serializable {
+        private static final long serialVersionUID = 2940665245810221108L;
+        private T message;
+        private ActorRef receiver;
+    }
 
-	private void handle(LargeMessage<?> largeMessage) {
-		Object message = largeMessage.getMessage();
-		ActorRef sender = this.sender();
-		ActorRef receiver = largeMessage.getReceiver();
-		ActorSelection receiverProxy = this.context().actorSelection(receiver.path().child(DEFAULT_NAME));
-		
-		// TODO: Implement a protocol that transmits the potentially very large message object.
-		// The following code sends the entire message wrapped in a BytesMessage, which will definitely fail in a distributed setting if the message is large!
-		// Solution options:
-		// a) Split the message into smaller batches of fixed size and send the batches via ...
-		//    a.a) self-build send-and-ack protocol (see Master/Worker pull propagation), or
-		//    a.b) Akka streaming using the streams build-in backpressure mechanisms.
-		// b) Send the entire message via Akka's http client-server component.
-		// c) Other ideas ...
-		// Hints for splitting:
-		// - To split an object, serialize it into a byte array and then send the byte array range-by-range (tip: try "KryoPoolSingleton.get()").
-		// - If you serialize a message manually and send it, it will, of course, be serialized again by Akka's message passing subsystem.
-		// - But: Good, language-dependent serializers (such as kryo) are aware of byte arrays so that their serialization is very effective w.r.t. serialization time and size of serialized data.
-		receiverProxy.tell(new BytesMessage<>(message, sender, receiver), this.self());
-	}
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class BytesMessage implements Serializable {
+        private static final long serialVersionUID = 4057807743872319842L;
+        private byte[] bytes;
+    }
 
-	private void handle(BytesMessage<?> message) {
-		// TODO: With option a): Store the message, ask for the next chunk and, if all chunks are present, reassemble the message's content, deserialize it and pass it to the receiver.
-		// The following code assumes that the transmitted bytes are the original message, which they shouldn't be in your proper implementation ;-)
-		message.getReceiver().tell(message.getBytes(), message.getSender());
-	}
+    /////////////////
+    // Actor State //
+    /////////////////
+    private static List<BytesMessage> data;
+    /////////////////////
+    // Actor Lifecycle //
+    /////////////////////
+
+    ////////////////////
+    // Actor Behavior //
+    ////////////////////
+
+    static class StreamInitialized {
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class StreamCompleted {
+        private final ActorRef receiver;
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class StreamFailure {
+        private final Throwable cause;
+    }
+
+    enum Ack {
+        INSTANCE;
+    }
+
+    @Override
+    public Receive createReceive() {
+        return receiveBuilder()
+                .match(LargeMessage.class, this::handle)
+                .match(
+                        StreamInitialized.class,
+                        init -> {
+                            log().info("Stream initialized");
+                            data = new ArrayList<>();
+                            sender().tell(Ack.INSTANCE, self());
+                        })
+                .match(
+                        BytesMessage.class,
+                        element -> {
+                            //log().info("Received element: {}", element);
+                            data.add(element);
+                            sender().tell(Ack.INSTANCE, self());
+                        })
+                .match(
+                        StreamCompleted.class, this::handle)
+                .match(
+                        StreamFailure.class,
+                        failed -> {
+                            log().error(failed.getCause(), "Stream failed!");
+                        })
+                .matchAny(object -> this.log().info("Received unknown message: \"{}\"", object.toString()))
+                .build();
+    }
+
+    private void handle(LargeMessage<?> largeMessage) {
+        Object message = largeMessage.getMessage();
+        ActorRef receiver = largeMessage.getReceiver();
+        ActorSelection receiverProxy = this.context().actorSelection(receiver.path().child(DEFAULT_NAME));
+        // TODO: Maybe change with communication
+
+        CompletionStage<ActorRef> newRef = receiverProxy.resolveOne(Duration.ofSeconds(1));
+        try {
+            ActorRef x = newRef.toCompletableFuture().get();
+
+            log().info("Start sending msg");
+
+            KryoPool serializer = KryoPoolSingleton.get();
+            byte[] output = serializer.toBytesWithClass(message);
+            Iterable<BytesMessage> groupedOutput = group(output, 100);
+
+            Source<BytesMessage, NotUsed> source = Source.from(groupedOutput);
+            Sink<BytesMessage, NotUsed> sink = Sink.actorRefWithBackpressure(x,
+                    new StreamInitialized(),
+                    Ack.INSTANCE,
+                    new StreamCompleted(receiver),
+                    ex -> new StreamFailure(ex));
+            source.runWith(sink, this.getContext().getSystem());
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void handle(StreamCompleted message) {
+        this.log().info("StreamCompleted");
+        KryoPool serializer = KryoPoolSingleton.get();
+        ActorRef receiver = message.getReceiver();
+
+        LargeMessage originalMessage = new LargeMessage(serializer.fromBytes(convertMessages()), receiver);
+        receiver.tell(originalMessage.getMessage(), getSelf());
+    }
+
+    private static byte[] convertMessages() {
+        int totalBytes = (data.size() - 1) * data.get(0).getBytes().length + data.get(data.size() - 1).getBytes().length;
+        byte[] bytes = new byte[totalBytes];
+        int i = 0;
+        for (BytesMessage m : data) {
+            System.arraycopy(m.getBytes(), 0, bytes, i, m.getBytes().length);
+            i += m.getBytes().length;
+        }
+        return bytes;
+    }
+
+    private static List<BytesMessage> group(byte[] byteArray, int size) {
+        List<BytesMessage> groupedBytes = new ArrayList<>();
+        for (int i = 0; i < byteArray.length; i += size) {
+            int sizeOfArray = Math.min(byteArray.length - i, size);
+            byte[] sliced = Arrays.copyOfRange(byteArray, i, i + sizeOfArray);
+            groupedBytes.add(new BytesMessage(sliced));
+        }
+        return groupedBytes;
+    }
+
 }
